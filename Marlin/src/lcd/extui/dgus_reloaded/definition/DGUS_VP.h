4598d2c0161c4307c6a6ceab16b0295d6a8c7fef
Jakub <jakub.fridrich@klik.cz>
2025-08-06 14:50:56 +0200

    same

--- Marlin/src/lcd/extui/dgus_reloaded/definition/DGUS_VP.h
+++ Marlin/src/lcd/extui/dgus_reloaded/definition/DGUS_VP.h
@@ -23,18 +23,18 @@
 
 #include "../config/DGUS_Addr.h"
 
-#define VPFLAG_NONE 0
-#define VPFLAG_AUTOUPLOAD (1U << 0) // Upload on every DGUS update
-#define VPFLAG_RXSTRING (1U << 1)   // Treat the received data as a string (terminated with 0xFFFF)
+#define VPFLAG_NONE        0
+#define VPFLAG_AUTOUPLOAD  (1U << 0) // Upload on every DGUS update
+#define VPFLAG_RXSTRING    (1U << 1) // Treat the received data as a string (terminated with 0xFFFF)
 
 struct DGUS_VP {
   DGUS_Addr addr;
-  uint8_t size;
-  uint8_t flags;
-  void *extra;
+  uint8_t   size;
+  uint8_t   flags;
+  void      *extra;
 
   // Callback that will be called if the display modified the value.
   // nullptr makes it readonly for the display.
-  void (*rx_handler)(DGUS_VP&, void*);
-  void (*tx_handler)(DGUS_VP&);
+  void (*rx_handler)(DGUS_VP &, void *);
+  void (*tx_handler)(DGUS_VP &);
 };
