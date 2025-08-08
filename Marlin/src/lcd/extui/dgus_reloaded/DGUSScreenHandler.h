f79b0fe0b710dd15d537df0cf0c6671b8718e39e
Jakub <jakub.fridrich@klik.cz>
2025-08-08 09:08:11 +0200

    formt screenhandler

--- Marlin/src/lcd/extui/dgus_reloaded/DGUSScreenHandler.h
+++ Marlin/src/lcd/extui/dgus_reloaded/DGUSScreenHandler.h
@@ -62,13 +62,13 @@ public:
     static void SDCardError();
 #endif
 
-#if ENABLED(POWER_LOSS_RECOVERY)
+  #if ENABLED(POWER_LOSS_RECOVERY)
     static void PowerLossResume();
-#endif
+  #endif
 
-#if HAS_PID_HEATING
+  #if HAS_PID_HEATING
     static void PidTuning(const ExtUI::result_t rst);
-#endif
+  #endif
 
     static void ShowPauseMessage(PauseMessage message, PauseMode mode);
 
@@ -80,23 +80,23 @@ public:
     static void SetTextSize(DGUS_Addr var, uint16_t len, const int16_t *boxSize, bool center = false);
     static void ShowWaitScreen(DGUS_Screen return_screen, bool has_continue = false);
 
-    static DGUS_Screen GetCurrentScreen();
-    static void TriggerScreenChange(DGUS_Screen screen);
-    static void TriggerFullUpdate();
+  static DGUS_Screen GetCurrentScreen();
+  static void TriggerScreenChange(DGUS_Screen screen);
+  static void TriggerFullUpdate();
 
-    static void TriggerEEPROMSave();
+  static void TriggerEEPROMSave();
 
-    static bool IsPrinterIdle();
+  static bool IsPrinterIdle();
 
-    static void MoveToLevelPoint();
+  static void MoveToLevelPoint();
 
     static uint8_t debug_count;
 
-#if ENABLED(SDSUPPORT)
+  #if ENABLED(SDSUPPORT)
     static ExtUI::FileList filelist;
     static uint16_t filelist_offset;
     static int16_t filelist_selected;
-#endif
+  #endif
 
     static DGUS_Data::StepSize offset_steps;
     static DGUS_Data::StepSize move_steps;
@@ -115,27 +115,27 @@ public:
 
     static char gcode[DGUS_GCODE_LEN + 1];
 
-    static DGUS_Data::Heater pid_heater;
-    static uint16_t pid_temp;
-    static uint8_t pid_cycles;
+  static DGUS_Data::Heater pid_heater;
+  static uint16_t pid_temp;
+  static uint8_t pid_cycles;
 
-    static bool wait_continue;
+  static bool wait_continue;
 
-    static bool leveling_active;
+  static bool leveling_active;
 
 private:
-    static const DGUS_Addr *FindScreenAddrList(DGUS_Screen screen);
-    static bool CallScreenSetup(DGUS_Screen screen);
+  static const DGUS_Addr *FindScreenAddrList(DGUS_Screen screen);
+  static bool CallScreenSetup(DGUS_Screen screen);
 
-    static void MoveToScreen(DGUS_Screen screen, bool abort_wait = false);
-    static bool SendScreenVPData(DGUS_Screen screen, bool complete_update);
+  static void MoveToScreen(DGUS_Screen screen, bool abort_wait = false);
+  static bool SendScreenVPData(DGUS_Screen screen, bool complete_update);
 
-    static bool settings_ready;
-    static bool booted;
+  static bool settings_ready;
+  static bool booted;
 
-    static DGUS_Screen current_screen;
-    static DGUS_Screen new_screen;
-    static bool full_update;
+  static DGUS_Screen current_screen;
+  static DGUS_Screen new_screen;
+  static bool full_update;
 
     static DGUS_Screen wait_return_screen;
 
@@ -153,11 +153,11 @@ private:
 extern DGUSScreenHandler dgus_screen_handler;
 
 extern const char DGUS_MSG_HOMING_REQUIRED[],
-    DGUS_MSG_BUSY[],
-    DGUS_MSG_UNDEF[],
-    DGUS_MSG_HOMING[],
-    DGUS_MSG_FW_OUTDATED[],
-    DGUS_MSG_ABL_REQUIRED[];
+                  DGUS_MSG_BUSY[],
+                  DGUS_MSG_UNDEF[],
+                  DGUS_MSG_HOMING[],
+                  DGUS_MSG_FW_OUTDATED[],
+                  DGUS_MSG_ABL_REQUIRED[];
 
 extern const char DGUS_CMD_HOME[],
-    DGUS_CMD_EEPROM_SAVE[];
+                  DGUS_CMD_EEPROM_SAVE[];
