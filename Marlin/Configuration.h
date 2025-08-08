468b28b9f64e80ea16518fb08d176e4d48d0e89a
Jakub <jakub.fridrich@klik.cz>
2025-08-08 14:48:01 +0200

    zmax

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1761,6 +1761,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
+//#define Z_MAX_POS 200
 #if defined(LK1_Pro) || defined(LK5_Pro)
   #define Z_MAX_POS 400
 #else
